07234bc60a4d28e50728fe825292f2556a623451
Bill Sanders <billysanders@gmail.com>
2022-08-25 18:52:31 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Test gitlab cicd

--- .github/scripts/oned.sh
+++ .github/scripts/oned.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and        #
 # limitations under the License.                                             #
 #--------------------------------------------------------------------------- #
-
+set -x
 # Set credentials
 sudo su -c "echo 'oneadmin:opennebula' > /var/lib/one/.one/one_auth" oneadmin
 
